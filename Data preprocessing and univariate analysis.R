79d3e37ed9b9b1df5ab204a208f5551f6d861789
PAIDAXING-HUP <QWRSGUYGA@protonmail.com>
2025-07-02 17:10:40 +0800

    Update Data preprocessing and univariate analysis.R

--- Data preprocessing and univariate analysis.R
+++ Data preprocessing and univariate analysis.R
@@ -18,279 +18,161 @@ summary(result)
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ renal_failure , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ renal_failure , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ neurological_disease , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ neurological_disease , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ crrt , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ crrt , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ ventilator_setting , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ ventilator_setting , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ vasopressin , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ vasopressin , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ midazolam_use , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ midazolam_use , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ fentanyl_use , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ fentanyl_use , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ age , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ age , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ weight , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ weight , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ sofa , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ sofa , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ temperature_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ temperature_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ heart_rate_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ heart_rate_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ resp_rate_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ resp_rate_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ sbp_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ sbp_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ dbp_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ dbp_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ mbp_mean , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ mbp_mean , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ hemoglobin_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ hemoglobin_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ platelets_max , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ platelets_max , family = binomial, data = data_frame) 
 summary(result)
- 
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ wbc_max , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ wbc_max , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ bun_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ bun_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ calcium_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ calcium_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ creatinine_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ creatinine_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ sodium_max , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ sodium_max , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ potassium_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ potassium_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ inr_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ inr_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ pt_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ pt_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ ptt_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ ptt_min , family = binomial, data = data_frame) 
 summary(result)
- 
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ lactate_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ lactate_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
- result <- glm( cam_icu_score ~ ph_min , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ ph_min , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ po2_max , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ po2_max , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
-
- result <- glm( cam_icu_score ~ pco2_max , family = binomial, data = data_frame) 
-
+result <- glm( cam_icu_score ~ pco2_max , family = binomial, data = data_frame) 
 summary(result)
-
 exp(coef(result))
 exp(confint.default(result))
 exp(confint(result))
-
 result <- glm(cam_icu_score ~  neurological_disease + ventilator_setting + midazolam_use  + sofa + temperature_mean + hemoglobin_min + sodium_max, family = binomial, data = data_frame)
 summary(result)
 exp(coef(result))
