fa2a824db7a5d075657976c929bf00b56e37a4b9
PAIDAXING-HUP <QWRSGUYGA@protonmail.com>
2025-07-02 16:58:33 +0800

    Update dca code.R

--- dca code.R
+++ dca code.R
@@ -1,18 +1,5 @@
-# R Code:
-
-# Load required packages
 library(rmda)
-
-
-# Fit logistic regression on training data
-# 在训练集上拟合逻辑回归模型
-
 fit.glm <- glm(cam_icu_score ~ neurological_disease + ventilator_setting + midazolam_use +     sofa + temperature_mean + hemoglobin_min + sodium_max, data = dca_train_data, family = binomial())
-# DCA style1 on training data
-
-
-# 训练集 DCA (风格1)
-
 dca1_train_data <- na.omit(dca_train_data[, c('cam_icu_score', 'neurological_disease', 'ventilator_setting', 'midazolam_use', 'sofa', 'temperature_mean', 'hemoglobin_min', 'sodium_max')])
 
 pred.logit_train <- predict(fit.glm, dca1_train_data)
@@ -26,7 +13,7 @@ DCA.1_train <- rmda::decision_curve(
   family = binomial(link = 'logit'),
 
   thresholds = seq(0, 1, by = 0.01),
-# DCA style1 on validation data
+
   confidence.intervals = 0.95,
   study.design = 'cohort',
   data = dca1_train_data
@@ -35,7 +22,6 @@ DCA.1_train <- rmda::decision_curve(
 plot.new()
 rmda::plot_decision_curve(DCA.1_train, curve.names = 'Model', xlim = c(0, 0.8), cost.benefit.axis = TRUE, col = '#E64B35B2', confidence.intervals = FALSE, standardize = FALSE)
 
-# 验证集 DCA (风格1)
 dca1_validation_data <- na.omit(dca_validation_data[, c('cam_icu_score', 'neurological_disease', 'ventilator_setting', 'midazolam_use', 'sofa', 'temperature_mean', 'hemoglobin_min', 'sodium_max')])
 pred.logit_val <- predict(fit.glm, dca1_validation_data)
 dca1_validation_data$pred.logit <- pred.logit_val
