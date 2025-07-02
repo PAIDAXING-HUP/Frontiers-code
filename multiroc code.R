ff6de22ccdc0a4ab60cbec4418ba44fa093bef3a
PAIDAXING-HUP <QWRSGUYGA@protonmail.com>
2025-07-02 17:12:12 +0800

    Update multiroc code.R

--- multiroc code.R
+++ multiroc code.R
@@ -35,7 +35,6 @@ label_train_cohort <- paste0('AUC = ', auc_fmt_train_cohort, ifelse(ci_fmt_train
 p_train_cohort <- p_train_cohort + annotate('text', x = 0.95, y = 0.05, label = label_train_cohort, color = '#E64B35FF', size = 4.5, hjust = 1)
 
 plot_train_cohort <- p_train_cohort
-
 pred_validation_cohort <- predict(fit, newdata = validation_cohort, type = 'link')
 validation_cohort$pred_value <- pred_validation_cohort
 roc_validation_cohort <- pROC::roc(validation_cohort$cam_icu_score, validation_cohort$pred_value, levels = c(0,1))
@@ -43,15 +42,12 @@ auc_validation_cohort <- pROC::auc(roc_validation_cohort)
 ci_temp_validation_cohort <- tryCatch({ci <- pROC::ci(roc_validation_cohort); if (any(is.na(ci))) NA else paste0('(', formatC(ci[1], format='f', digits = 3), '-', formatC(ci[3], format='f', digits = 3), ')')}, error = function(e) NA)
 auc_fmt_validation_cohort <- formatC(auc_validation_cohort, format = 'f', digits = 3)
 ci_fmt_validation_cohort <- ifelse(is.na(ci_temp_validation_cohort), '', ci_temp_validation_cohort)
-
 auc_results <- base::rbind(auc_results, data.frame(
   Dataset = 'validation_cohort',
   AUC     = auc_fmt_validation_cohort,
   CI      = ci_fmt_validation_cohort,
   stringsAsFactors = FALSE
 ))
-
-
 p_validation_cohort <- pROC::ggroc(roc_validation_cohort, legacy.axes = TRUE, size = 0.7, color = '#4DBBD5FF') +
   geom_abline(slope = 1, intercept = 0, color = 'grey', size = 1) +
   coord_fixed() +
@@ -67,12 +63,9 @@ p_validation_cohort <- pROC::ggroc(roc_validation_cohort, legacy.axes = TRUE, si
     plot.margin = margin(10, 10, 20, 10)
   ) +
   labs(x = 'False positive rate (1 − specificity)', y = 'True positive rate (sensitivity)')
-
 label_validation_cohort <- paste0('AUC = ', auc_fmt_validation_cohort, ifelse(ci_fmt_validation_cohort == '', '', paste0(' ', ci_fmt_validation_cohort)))
 p_validation_cohort <- p_validation_cohort + annotate('text', x = 0.95, y = 0.05, label = label_validation_cohort, color = '#4DBBD5FF', size = 4.5, hjust = 1)
-
 plot_validation_cohort <- p_validation_cohort
-
 roc_list <- list('train_cohort' = roc_train_cohort, 'validation_cohort' = roc_validation_cohort)
 p_Combined <- pROC::ggroc(roc_list, legacy.axes = TRUE, size = 0.7) +
   scale_color_manual(values = c('#E64B35FF', '#4DBBD5FF'), labels = c('Training cohort', 'Internal test cohort')) +
@@ -92,16 +85,11 @@ p_Combined <- pROC::ggroc(roc_list, legacy.axes = TRUE, size = 0.7) +
   ) +
   labs(x = 'False positive rate (1 − specificity)', y = 'True positive rate (sensitivity)') +
   theme(legend.position = 'right', legend.title = element_blank())
-
 label_combined_train_cohort <- paste0('AUC = ', auc_results$AUC[1], ifelse(auc_results$CI[1] == '', '', paste0(' ', auc_results$CI[1])))
 p_Combined <- p_Combined + annotate('text', x = 0.95, y = 0.25, label = label_combined_train_cohort, color = '#E64B35FF', size = 4.5, hjust = 1)
-
 label_combined_validation_cohort <- paste0('AUC = ', auc_results$AUC[2], ifelse(auc_results$CI[2] == '', '', paste0(' ', auc_results$CI[2])))
 p_Combined <- p_Combined + annotate('text', x = 0.95, y = 0.2, label = label_combined_validation_cohort, color = '#4DBBD5FF', size = 4.5, hjust = 1)
-
 plot_Combined <- p_Combined
-
-
 print(auc_results)
 
 
